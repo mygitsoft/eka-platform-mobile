580047f817cb9f48006a0338f0327fabbf6555a4
shps777@gmail.com <shps777@gmail.com>
2026-09-25 22:07:58 +0530

    Minified code

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'oxc',
+    cssMinify: 'lightningcss',
+  },
   server: {
     host: '0.0.0.0',
   },
